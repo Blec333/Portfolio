409455d5f0b86bed6745581949fe981831a3e2e2
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-06-24 14:52:03 -0700

    remove gh-pages homepage

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
           <div className="flex-none">
             <ul className="menu menu-horizontal p-0">
               <li tabIndex="0">
-                <a href="#aboutme">Pages<svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg></a>
+                <a href="/">Pages<svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg></a>
                 <ul className="p-2 bg-neutral text-primary-content">
                   <li className="text-lg text-color-primary px-2 mx-2">
                     <Link to="/aboutme" className={window.location.pathname === '/aboutme' ? 'nav-link active' : 'nav-link'}>About Me</Link>
