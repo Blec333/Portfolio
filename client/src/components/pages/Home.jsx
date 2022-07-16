25f1c94599222cd90fda19197797b45821e4283c
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-07-16 14:41:47 -0700

    fix spelling on home

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -26,7 +26,7 @@ export default function Home() {
         </div>
       </div>
       <div className="bg-transparent text-center">
-        <div className="text-primary-content text-center">Welcome to my portfolio!  This site is an example of a MERN fullstack application backed by MongoDB.  I'm currently open to full-time and contract opportunities in software development including MERN sack, VBA, Lisp, C#.NET, and Dynamo.  You will find a download link to my resume available in the header as well as on the resume page. If you wish to contact me you may reach out from the contact me page.
+        <div className="text-primary-content text-center">Welcome to my portfolio!  This site is an example of a MERN fullstack application backed by MongoDB.  I'm currently open to full-time and contract opportunities in software development including MERN stack, VBA, Lisp, C#.NET, and Dynamo.  You will find a download link to my resume available in the header as well as on the resume page. If you wish to contact me you may reach out from the contact me page.
         </div>
       </div>
     </div>
