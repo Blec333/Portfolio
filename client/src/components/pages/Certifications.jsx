4a6458056ec9e9dd0cfbb125e4fc9696b1f885a8
Brennan LeClair <94870273+Blec333@users.noreply.github.com>
2022-07-18 20:43:57 -0700

    add bachelors degree to certs page

--- client/src/components/pages/Certifications.jsx
+++ client/src/components/pages/Certifications.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import profilePic from "../../img/Profile-Picture.png";
 import fullstackCertImg from "../../img/fullstack-cert.jpg";
+import bachelorsImg from "../../img/bachelors.jpg";
 
 export default function Home() {
 
@@ -10,15 +11,20 @@ export default function Home() {
     <div className="bg-transparent w-full justify-center text-center">
       <br />
       <figure>
-        <div className="flex justify-center">
+        <div className="flex justify-center py-8">
           <div className="w-24 h-24 rounded-[33px] overflow-hidden">
             <img alt="Headshot" src={profilePic} />
           </div>
         </div>
       </figure>
-      <div className="flex flex-col items-center bg-transparent text-center">
+      <div className="flex flex-col items-center bg-transparent text-center py-8">
         <div className="w-2/3 h-auto rounded-box overflow-hidden">
           <img alt="Certification" src={fullstackCertImg} />
+        </div>
+      </div>
+      <div className="flex flex-col items-center bg-transparent text-center py-8">
+        <div className="w-2/3 h-auto rounded-box overflow-hidden">
+          <img alt="Certification" src={bachelorsImg} />
         </div>
       </div>
     </div>
